bb17c76f59482574af24ae3f57df92f59b0f6e3e
faber258 <18377101@buaa.edu.cn>
2020-03-30 19:36:39 +0800

    more functions

--- fileinto.py
+++ fileinto.py
@@ -42,3 +42,38 @@ def go():
         d3.pack()
         d4.pack()
         inp.mainloop()
+def enter():
+    x=0
+    mingzi=username.get()
+    mima=password.get()
+    if mingzi in dic:
+            if dic[mingzi]['Pwd']==mima:
+                x=1
+    if x==1:
+         tkinter.messagebox.showinfo('登陆成功','欢迎教师'+mingzi)
+         top.destroy()
+         global sec
+         sec=Tk()
+         sec.title('学生管理系统')
+         b1=Button(sec,text='添加成绩',command=lambda:increase())
+         b2=Button(sec,text='查看成绩',command=lambda:look())
+         b3=Button(sec,text='退出系统',command=sec.destroy)
+         b1.grid(row=0,padx=100,pady=100)
+         b2.grid(row=2,padx=100,pady=100)
+         b3.grid(row=4,padx=100,pady=100)
+         sec.mainloop()
+    else:
+        tkinter.messagebox.showinfo('登录失败','用户名或密码错误')
+def go2():
+    newname=c1.get()
+    grade=d2.get()
+    sl[newname]=grade
+    tkinter.messagebox.showinfo('提示','添加成功！')
+    inp.destroy()
+    inc.destroy()
+
+def look():
+    key_list = [item for item in sl.keys()]
+    value_list = [item for item in sl.values()]
+    for i in range(len(key_list)):
+        print('姓名：%s   成绩：%s  ' % (key_list[i],value_list[i]))
