1e39624c627af1b11682f824830096003d4d63c5
clearlove258 <2754665078@qq.com>
2020-03-30 16:50:36 +0800

    initial update

--- fileinto.py
+++ fileinto.py
@@ -1,47 +1,4 @@
-int saddle(int m, int n, int a[][3], int ***q)
+from tkinter import*
 
-{
-
-	int **res;
-
-	res=(int**)malloc(sizeof(int*)*(m>n?n:m));
-
-	int i,j,k,min,min_index,b,s=0;
-
-	for(i=0;i<m;i++){
-
-		min=a[i][0];
-
-		min_index=0;
-
-		for(j=1;j<n;j++){
-
-			if(a[i][j]<min){
-
-				min=a[i][j];
-
-				min_index=j;
-
-			}
-
-		}
-
-		for(k=0;k<m;k++){
-
-			if(a[k][min_index]>min) break;
-
-		}
-
-		
-
-		}
-
-	}
-
-	*q=res;
-
-	return s;
-
-}
-
+import tkinter.messagebox
 
