bffa1f730e17a608b32d4def1ced9c5f55d70be9
faber258 <18377101@buaa.edu.cn>
2020-03-30 19:11:39 +0800

    update by the second developer

--- fileinto.py
+++ fileinto.py
@@ -1,4 +1,12 @@
 from tkinter import*
-
 import tkinter.messagebox
-
+top=Tk()
+dic={'admin':{'Name':'adnmb','Pwd':'4396'}}
+sl={'帅气助教1':100,'帅气助教2':100,'漂酿助教':100}
+top.title('学生管理系统登录界面')
+label1=Label(top,text='用户名')
+label2=Label(top,text='密码')
+username=Entry(top)
+password=Entry(top)
+enter1=Button(top,text='Log in',command=lambda:enter())
+out=Button(top,text='退出',command=top.destroy)
